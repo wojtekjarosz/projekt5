a64744e8b982314bc83a488e34fc8f0d1a31eeba
Someone <someone@someplace.com>
2016-05-24 20:01:45 +0200

    Wysylanie sygnalu / makefile

--- pro5.c
+++ pro5.c
@@ -2,14 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
+
+struct buf_elem {
+	long mtype;
+	char mvalue[40];
+   };
 
 int main(){
 	char z[40];
-int i=0,f,k=0;
+	int i=0,f,k=0;
 	while((!feof(stdin) )&& (k==0)){
 		f=fread(&z[i],1,1,stdin);
 		if (z[i]==10) k=1;
 		i++;
 		}
+	
+	
+	int msgid,msgid2;
+	struct buf_elem elem;
+	elem.mtype=123;
+	sprintf(elem.mvalue,z);
+	msgid = msgget(51212, IPC_CREAT|IPC_EXCL|0600);
+	if (msgid == -1){
+		msgid = msgget(51212, IPC_CREAT|0600);
+		if (msgid == -1){
+			perror("Utworzenie kolejki komunikatów");
+			exit(1);
+		}
+	}
+	if( msgsnd(msgid, &elem , 40 , 0) == -1) {
+		perror("msgsnd");
+		exit(1);
+	}
 }
